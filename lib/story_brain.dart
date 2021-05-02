63c3b6d26a23e2eb7dec6c204233e229457a3dd9
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-02 19:12:58 +0200

    add a method to hide second button when storyNumber is 3, 4 or 5

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -66,6 +66,6 @@ class StoryBrain {
 
   void restart() => _storyNumber = 0;
 
+  /// return true if _storyNumber == 0, 1 or 2
+  bool buttonShouldBeVisible() => (_storyNumber < 3);
 }
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
